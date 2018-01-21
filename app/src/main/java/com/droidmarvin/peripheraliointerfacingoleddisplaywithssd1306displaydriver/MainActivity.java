9d854f950b08ceec2ed9156b9909b8de0b5b4f8b
ngesa marvin <marvinngesa@gmail.com>
2018-01-21 11:24:38 +0300

    things initial commit

---
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingoleddisplaywithssd1306displaydriver/MainActivity.java
@@ -0,0 +1,11 @@
+package com.droidmarvin.peripheraliointerfacingoleddisplaywithssd1306displaydriver;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
