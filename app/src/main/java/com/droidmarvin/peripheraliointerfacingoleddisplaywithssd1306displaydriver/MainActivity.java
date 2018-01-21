cf26de180c7aa083dc2454190be23d5eb480a163
ngesa marvin <marvinngesa@gmail.com>
2018-01-21 12:24:16 +0300

    setup the OLED Display

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingoleddisplaywithssd1306displaydriver/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingoleddisplaywithssd1306displaydriver/MainActivity.java
@@ -2,10 +2,35 @@ package com.droidmarvin.peripheraliointerfacingoleddisplaywithssd1306displaydriv
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.contrib.driver.ssd1306.Ssd1306;
+
+import java.io.IOException;
+
 public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String I2C_OLED_BUS = "I2C1";
+    private Ssd1306 mOled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    //setup the OLED Display
+    private void setupOledDisplay() {
+        try {
+            mOled = new Ssd1306(I2C_OLED_BUS);
+        } catch (IOException e) {
+            Log.e(TAG, "Error while opening screen", e);
+        }
+        Log.d(TAG, "OLED screen activity created");
+    }
+
+
 }
+
+
